7e6751c1b3d9e9411b878c30e7494adabf1a32ed
Yashkin Andrey <yashyaaah52@gmail.com>
2026-05-10 20:46:53 +0300

    add module for workimg with clients

---
+++ models/client.py
@@ -0,0 +1,34 @@
+from database.db_manager import get_connection
+
+class Client:
+    def __init__(self, client_id=None, name=None, passport=None, phone=None, reliability="Высокая"):
+        self.id = client_id
+        self.name = name
+        self.passport = passport
+        self.phone = phone
+        self.reliability = reliability
+
+    def save(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        if self.id is None:
+            cursor.execute("""
+                INSERT INTO Client (name, passportData, phoneNumber, reliabilityStatus)
+                VALUES (?, ?, ?, ?)
+            """, (self.name, self.passport, self.phone, self.reliability))
+            self.id = cursor.lastrowid
+        else:
+            cursor.execute("""
+                UPDATE Client SET name = ?, passportData = ?, phoneNumber = ?, reliabilityStatus = ?
+                WHERE ClientID = ?
+            """, (self.name, self.passport, self.phone, self.reliability, self.id))
+        conn.commit()
+        conn.close()
+
+def get_all_clients():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT ClientID, name, passportData, phoneNumber, reliabilityStatus FROM Client")
+    rows = cursor.fetchall()
+    conn.close()
+    return [Client(*row) for row in rows]
